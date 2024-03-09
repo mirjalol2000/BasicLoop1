529cfcdf2e6a7cc13894ab12c9dee8018b6a3641
hicadok105@ridteam.com <hicadok105@ridteam.com>
2024-03-09 23:38:39 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sum = 0;
+
+for(int i = 0; i < 1000; i++)
+{
+    if (i % 2 == 0)
+    {
+        sum += i;
+    }
+}
+Console.WriteLine(sum);
